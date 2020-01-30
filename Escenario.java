06ba0fb0a284089df1252d58c30089475266afa0
Ivan Uresti <ivan.uresti@gmail.com>
2020-01-30 08:59:39 -0600

    Intercambio inicial de proyecto

---
+++ Escenario.java
@@ -0,0 +1,29 @@
+/**
+ * Write a description of class Escenario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escenario
+{
+    // instance variables - replace the example below with your own
+    private boolean dia;
+    
+   
+    /**
+     * Constructor for objects of class Escenario
+     */
+    public Escenario()
+    {
+        // initialise instance variables
+    }
+
+    
+    public void setDia(boolean dia){
+        this.dia = dia;
+    }
+    
+    public boolean isDia(){
+        return dia;
+    }
+}
---
+++ Jugador.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Jugador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jugador
+{
+    // instance variables - replace the example below with your own
+    private int currentFrame;
+    
+
+    /**
+     * Constructor for objects of class Jugador
+     */
+    public Jugador()
+    {
+        // initialise instance variables
+
+    }
+
+   
+}
